94c6c4e58db9570b23951b9df1c97ef5f29d3f66
Sachin Kumar Upadhyay <137228483+SACHINUPADHYAY414@users.noreply.github.com>
2023-08-01 13:00:33 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const API = axios.create({ baseURL: 'http://localhost:5000'});
-const API = axios.create({ baseURL: 'https://sachin-0ras.onrender.com/'});
+const API = axios.create({ baseURL:'https://sachinupadhyay.onrender.com'});
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('Profile')){
         req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
